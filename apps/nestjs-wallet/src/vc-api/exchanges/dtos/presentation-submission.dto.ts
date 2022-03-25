4267e816c5daf0385a6034cee7475d11553f8dc7
John Henderson <john.henderson@energyweb.org>
2022-03-25 08:18:08 +0100

    add type to pres-sub validator

--- apps/nestjs-wallet/src/vc-api/exchanges/dtos/presentation-submission.dto.ts
+++ apps/nestjs-wallet/src/vc-api/exchanges/dtos/presentation-submission.dto.ts
@@ -1,7 +1,7 @@
-import { Column } from 'typeorm';
 import { VerificationResultDto } from '../../credentials/dtos/verification-result.dto';
 import { ValidateNested } from 'class-validator';
 import { VerifiablePresentationDto } from '../../credentials/dtos/verifiable-presentation.dto';
+import { Type } from 'class-transformer';
 
 /**
  * Presentation Submission Dto.
@@ -11,11 +11,13 @@ export class PresentationSubmissionDto {
    * The result of the verification of the submitted VP
    */
   @ValidateNested()
+  @Type(() => VerificationResultDto)
   verificationResult: VerificationResultDto;
 
   /**
    * The Verifiable Presentation submitted in response to the transaction's VP Request
    */
   @ValidateNested()
+  @Type(() => VerifiablePresentationDto)
   vp: VerifiablePresentationDto;
 }
