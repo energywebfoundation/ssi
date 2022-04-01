6d5e4b9c05b8d9e6c6127cff622083aca1f934bc
John Henderson <john.henderson@energyweb.org>
2022-04-01 16:30:43 +0200

    refactor(exchange.service): rename vcApiService

--- apps/nestjs-wallet/src/vc-api/exchanges/exchange.service.spec.ts
+++ apps/nestjs-wallet/src/vc-api/exchanges/exchange.service.spec.ts
@@ -19,7 +19,7 @@ const baseUrl = 'https://test-exchange.com';
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
-  let vcApiService: CredentialsService;
+  let credentialsService: CredentialsService;
   let exchangeRepository: Repository<ExchangeEntity>;
   let vpRequestRepository: Repository<VpRequestEntity>;
 
@@ -53,7 +53,7 @@ describe('ExchangeService', () => {
       ]
     }).compile();
 
-    vcApiService = module.get<CredentialsService>(CredentialsService);
+    credentialsService = module.get<CredentialsService>(CredentialsService);
     service = module.get<ExchangeService>(ExchangeService);
     exchangeRepository = module.get<Repository<ExchangeEntity>>(getRepositoryToken(ExchangeEntity));
     vpRequestRepository = module.get<Repository<VpRequestEntity>>(getRepositoryToken(VpRequestEntity));
@@ -117,6 +117,36 @@ describe('ExchangeService', () => {
       };
       await service.createExchange(exchangeDef);
       const exchangeResponse = await service.startExchange(exchangeId);
+      expect(exchangeResponse.vpRequest.interact.service).toHaveLength(1);
+      expect(exchangeResponse.vpRequest.interact.service[0].serviceEndpoint).toContain(baseUrl);
+    });
+  });
+
+  describe('continueExchange', () => {
+    it('should send transaction dto if callback is configured', async () => {
+      const transactionId = 'test-tx';
+      // const exchangeDef: ExchangeDefinitionDto = {
+      //   exchangeId: exchangeId,
+      //   interactServices: [
+      //     {
+      //       type: VpRequestInteractServiceType.unmediatedPresentation
+      //     }
+      //   ],
+      //   query: [],
+      //   isOneTime: false,
+      //   callback: []
+      // };
+      const vp = {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          'https://www.w3.org/2018/credentials/examples/v1'
+        ],
+        type: ['VerifiablePresentation'],
+        verifiableCredential: [],
+        holder: 'did:key:z6MksBH4LMy8SoYFUNjDXtQ2Rq4dHnyuemowxXqzLpuB6nvc',
+        proof: {}
+      };
+      const exchangeResponse = await service.continueExchange(vp, transactionId);
       expect(exchangeResponse.vpRequest.interact.service).toHaveLength(1);
       expect(exchangeResponse.vpRequest.interact.service[0].serviceEndpoint).toContain(baseUrl);
     });
