ff04ae7865dbc6eca4bb5cb2ceedc101b5662297
John Henderson <john.henderson@energyweb.org>
2022-04-01 15:46:49 +0200

    chore(rebeam.e2e): remove useless comment

--- apps/nestjs-wallet/test/vc-api/exchanges/rebeam/rebeam.e2e-suite.ts
+++ apps/nestjs-wallet/test/vc-api/exchanges/rebeam/rebeam.e2e-suite.ts
@@ -20,11 +20,6 @@ export const rebeamExchangeSuite = () => {
     const callbackUrlBase = 'http://example.com';
     const callbackUrlPath = '/endpoint';
     const presentationExchange = new RebeamCpoNode(`${callbackUrlBase}${callbackUrlPath}`);
-    // const expectedResponseBody = {
-    //   exchangeId: presentationExchange.getExchangeId(),
-    //   submittedVps: [],
-    //   result:
-    // }
     const scope = nock(callbackUrlBase).post(callbackUrlPath).reply(201);
     const exchangeDef = presentationExchange.getExchangeDefinition();
     await request(app.getHttpServer()).post(`${vcApiBaseUrl}/exchanges`).send(exchangeDef).expect(201);
