0b286c3a691f257a2e79f2c2e6cde35a6197c289
John Henderson <john.henderson@energyweb.org>
2022-06-09 12:35:26 +0200

    test(exchange): figure continue expectation config

--- apps/vc-api/test/vc-api/exchanges/rebeam/rebeam.e2e-suite.ts
+++ apps/vc-api/test/vc-api/exchanges/rebeam/rebeam.e2e-suite.ts
@@ -68,7 +68,7 @@ export const rebeamExchangeSuite = () => {
     const vp = await walletClient.provePresentation({ presentation, options: presentationOptions });
 
     // Holder submits presentation
-    await walletClient.continueExchange(presentationExchangeContinuationEndpoint, vp, false, true);
+    await walletClient.continueExchange(presentationExchangeContinuationEndpoint, vp, false);
     scope.done();
   });
 
