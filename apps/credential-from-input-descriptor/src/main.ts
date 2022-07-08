1a64b0ff378da4c3813b9c6d906df542eeb8a44e
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-07-08 18:42:24 +0200

    feat: adding swagger

--- apps/credential-from-input-descriptor/src/main.ts
+++ apps/credential-from-input-descriptor/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwaggerDocument(app);
   await app.listen(3000);
 }
+
 bootstrap();
+
+function setupSwaggerDocument(app: INestApplication): OpenAPIObject {
+  const config = new DocumentBuilder()
+    .setTitle('Input Descriptor to Credential converter')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  return document;
+}
