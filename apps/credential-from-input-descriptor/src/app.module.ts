e5294a7ad003e949aa3867aca92b797b499e1d33
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-07-14 10:26:28 +0200

    chore: removing empty controller and service

--- apps/credential-from-input-descriptor/src/app.module.ts
+++ apps/credential-from-input-descriptor/src/app.module.ts
@@ -1,13 +1,11 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AccessLog } from './middlewares/access-log.middleware';
 import { ConverterModule } from './modules/converter/converter.module';
 
 @Module({
   imports: [ConverterModule],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [],
+  providers: []
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
