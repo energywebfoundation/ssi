1330e6fdbe76e357b2a2fd3995c6b69182b5d748
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-07-27 15:06:50 +0200

    feat: forbidding submission of VP by DID non-matching the DID that initially submitted the presentation

--- apps/vc-api/src/vc-api/exchanges/exchange.service.ts
+++ apps/vc-api/src/vc-api/exchanges/exchange.service.ts
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { ConflictException, Injectable, Logger } from '@nestjs/common';
+import { ConflictException, ForbiddenException, Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -106,6 +106,13 @@ export class ExchangeService {
       };
     }
     const transaction = transactionQuery.transaction;
+
+    if (
+      transaction.presentationSubmission &&
+      transaction.presentationSubmission.vp.holder !== verifiablePresentation.holder
+    ) {
+      throw new ForbiddenException('DID does not match the DID that initially submitted the presentation');
+    }
 
     const { response, callback } = await transaction.processPresentation(
       verifiablePresentation,
