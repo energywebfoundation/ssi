47ee097a70f580e80124431047d88857ad787444
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-07-28 13:47:44 +0200

    feat: adding a logger to the TransactionEntityExceptionFilter

--- apps/vc-api/src/exception-filters/transaction-entity-exception.filter.ts
+++ apps/vc-api/src/exception-filters/transaction-entity-exception.filter.ts
@@ -17,17 +17,21 @@
 
 import { BaseExceptionFilter } from '@nestjs/core';
 import { TransactionEntityException } from '../vc-api/exchanges/entities/transaction.entity';
-import { ArgumentsHost, Catch, ForbiddenException } from '@nestjs/common';
+import { ArgumentsHost, Catch, ForbiddenException, Logger } from '@nestjs/common';
 
 @Catch(TransactionEntityException)
 export class TransactionEntityExceptionFilter extends BaseExceptionFilter {
+  private readonly logger: Logger = new Logger(TransactionEntityExceptionFilter.name, { timestamp: true });
+
   catch(exception: TransactionEntityException, host: ArgumentsHost): void {
     switch (exception.name) {
       case 'TransactionDidForbiddenException':
+        this.logger.warn(exception);
         super.catch(new ForbiddenException(exception.message), host);
         break;
 
       default:
+        this.logger.error(exception);
         super.catch(exception, host);
         break;
     }
