c4940fb30a8596a657038454fa2f515203882e86
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-09-02 12:03:21 +0200

    chore: updating all project .eslintrc.js files

--- apps/vc-api/.eslintrc.js
+++ apps/vc-api/.eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  parserOptions: { tsconfigRootDir: __dirname }
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+  },
+  extends: ['@energyweb'],
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
 };
--- libraries/did/.eslintrc.js
+++ libraries/did/.eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  parserOptions: { tsconfigRootDir: __dirname, ecmaVersion: 2018 }
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+  },
+  extends: ['@energyweb'],
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
 };
