5a0441e77efba8c79d71f017f96a9c894b70d83b
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-09-11 19:22:05 +0200

    fix: fixing validations not enforcing fields to be non empty objects in KeyPairDto

--- apps/vc-api/src/key/dtos/key-pair.dto.ts
+++ apps/vc-api/src/key/dtos/key-pair.dto.ts
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { IsObject } from 'class-validator';
+import { IsNotEmptyObject } from 'class-validator';
 import { JWK } from 'jose';
 import { ApiProperty } from '@nestjs/swagger';
 
@@ -23,11 +23,11 @@ import { ApiProperty } from '@nestjs/swagger';
  * KeyPair
  */
 export class KeyPairDto {
-  @IsObject()
+  @IsNotEmptyObject()
   @ApiProperty({ description: 'private key JWK' })
   public privateKey: JWK;
 
-  @IsObject()
+  @IsNotEmptyObject()
   @ApiProperty({ description: 'public key JWK' })
   public publicKey: JWK;
 }
