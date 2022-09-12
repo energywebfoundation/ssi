29138fbff8c6d7e4f7065f6e290814430ce8cf8a
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-09-12 09:19:21 +0200

    docs: adding TODO comment

--- apps/vc-api/src/key/dtos/key-pair.dto.ts
+++ apps/vc-api/src/key/dtos/key-pair.dto.ts
@@ -25,6 +25,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class KeyPairDto {
   @IsNotEmptyObject()
   @ApiProperty({ description: 'private key JWK' })
+  // TODO: decide if to reuse JsonWebKeyDto or define a new Dto class with more fields like in JWK interface from jose
   public privateKey: JWK;
 
   @IsNotEmptyObject()
