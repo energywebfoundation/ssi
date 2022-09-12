a951cb438661bee264fca163e9e35fd19c2fb3f5
John Henderson <john.henderson@energyweb.org>
2022-09-12 11:43:06 +0200

    fix(did.controller): no plans to use DID labeling
    Best to remove DID labels for now to avoid confusion

--- apps/vc-api/src/did/did.controller.ts
+++ apps/vc-api/src/did/did.controller.ts
@@ -59,17 +59,4 @@ export class DIDController {
   async getVerificationMethods(@Param('id') id: string): Promise<VerificationMethod> {
     throw new Error('Not implemented');
   }
-
-  /**
-   * Update the descriptive information information related to a DID
-   * "label" is kept in DID module because there is a one to one mapping between a DID and its label
-   * @param did
-   * @param label
-   * @param description
-   * @returns A DIDLabel entity
-   */
-  @Post('/label/:did')
-  async label(did: string, label: string, description: string) {
-    throw new Error('Not implemented');
-  }
 }
