78060ab824f0f816d5e03e48ef812a30fb3c7286
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-12-19 15:33:14 +0100

    fix: fixing BASE_URL that should be URI not hostname

--- apps/vc-api/src/config/env-vars-validation-schema.ts
+++ apps/vc-api/src/config/env-vars-validation-schema.ts
@@ -20,5 +20,5 @@ import * as Joi from 'joi';
 export const envVarsValidationSchema = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
   PORT: Joi.number().integer().positive().default(3000),
-  BASE_URL: Joi.string().hostname().default('http://localhost:3000')
+  BASE_URL: Joi.string().uri().default('http://localhost:3000')
 });
