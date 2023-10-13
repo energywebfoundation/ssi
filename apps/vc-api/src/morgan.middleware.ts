1a0a3ad3ae43d0bc7301e99accfbf8a06493a1ae
John Henderson <jrhender@users.noreply.github.com>
2023-10-13 16:41:20 +0100

    Revert "feat(vc-api): log req body (#188)" (#189)
    This reverts commit 244eec7fc608e1432741046d1fad3c32fff23006.

--- apps/vc-api/src/morgan.middleware.ts
+++ apps/vc-api/src/morgan.middleware.ts
@@ -1,14 +1,9 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import * as morgan from 'morgan';
-import { morganFormat } from './logging.config';
 
 @Injectable()
 export class MorganMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    if (req.baseUrl !== '' && req.baseUrl !== '/') {
-      morgan(morganFormat)(req, res, next);
-    } else {
-      next();
-    }
+    morgan('combined')(req, res, next);
   }
 }
