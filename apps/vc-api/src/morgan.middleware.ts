244eec7fc608e1432741046d1fad3c32fff23006
John Henderson <jrhender@users.noreply.github.com>
2023-10-13 16:17:09 +0100

    feat(vc-api): log req body (#188)
    * initial log of request body
    
    * don't log root

--- apps/vc-api/src/morgan.middleware.ts
+++ apps/vc-api/src/morgan.middleware.ts
@@ -1,9 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import * as morgan from 'morgan';
+import { morganFormat } from './logging.config';
 
 @Injectable()
 export class MorganMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    morgan('combined')(req, res, next);
+    if (req.baseUrl !== '' && req.baseUrl !== '/') {
+      morgan(morganFormat)(req, res, next);
+    } else {
+      next();
+    }
   }
 }
